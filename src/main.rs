2c2ed9dc48bd680ed296c011920eb2b34316a062
daniel <contact@dnlzrgz.com>
2026-06-21 18:27:15 +0200

    feat: init commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use ratatui::{DefaultTerminal, Frame};
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    ratatui::run(app)?;
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
+}
